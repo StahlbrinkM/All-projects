01699fdde9df1287a2961f53d3543da00c4dd687
StahlbrinkM <marcus_ow@hotmail.com>
2022-06-16 11:58:47 +0200

    Made code cleaner + added highlight to leader

--- Basketball scoreboard/script.js
+++ Basketball scoreboard/script.js
@@ -4,6 +4,10 @@ const guestScoreEl = document.getElementById("guest-score")
 let homeScore = 0
 let guestScore = 0
 
+const homeScoreDisplay = document.getElementById("home-score-container")
+const guestScoreDisplay = document.getElementById("guest-score-container")
+
+/* ---- OLD CODE ----
 
 function addScoreHomeOne() {
     homeScore += 1
@@ -34,10 +38,47 @@ function addScoreGuestThree() {
    guestScore += 3
    guestScoreEl.textContent = guestScore
 }
+*/
+
+// ---- NEW CODE ----
+
+// ---- Adding Scores ----
+
+function addScoreHome(points) {
+    homeScore += points
+    homeScoreEl.textContent = homeScore
+    highlightLeader()
+}
+
+function addScoreGuest(points) {
+    guestScore += points
+    guestScoreEl.textContent = guestScore
+    highlightLeader()
+}
+
+
+// ---- New game function ----
 
 function startNewGame() {
     homeScore = 0
     guestScore = 0
     homeScoreEl.textContent = homeScore
     guestScoreEl.textContent = guestScore
+    homeScoreDisplay.classList.remove('highlight')
+    guestScoreDisplay.classList.remove('highlight') 
+}
+
+// ---- Highlighting  the leader ----
+
+function highlightLeader() {
+    if (homeScore > guestScore) {
+        homeScoreDisplay.classList.add("highlight")
+        guestScoreDisplay.classList.remove("highlight")
+    } else if (guestScore > homeScore) {
+        guestScoreDisplay.classList.add("highlight")
+        homeScoreDisplay.classList.remove("highlight")
+    } else {
+        homeScoreDisplay.classList.remove("highlight")
+        guestScoreDisplay.classList.remove("highlight")
+    }
 }
